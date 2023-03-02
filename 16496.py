e4cc5e97d653de8035465ba067f9c955cf94e7b2
Starcea <stardev.uwu@gmail.com>
2023-03-02 14:06:52 +0900

    feat: 16496

---
+++ 16496.py
@@ -0,0 +1,8 @@
+from functools import cmp_to_key
+from sys import stdin
+input = stdin.readline
+
+input()
+a = input().split()
+a.sort(key=cmp_to_key(lambda x, y: -1 if int(x + y) > int(y + x) else 1))
+print(0 if sum(map(int, a)) == 0 else "".join(a))
